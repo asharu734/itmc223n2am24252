4f35646c28709795063214eeecae557598d9a497
soyluuj <juliusbragas212@gmail.com>
2025-05-20 17:54:11 +0800

    penis

--- src/coinflip/loginMenu.py
+++ src/coinflip/loginMenu.py
@@ -1,15 +1,24 @@
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, CENTER
+from toga import Box, Button, ImageView, Label, Image
+import os
 
 class LoginMenu(toga.Box):
     def __init__(self, app, **style):
         super().__init__(style=Pack(direction=COLUMN, padding=20, alignment=CENTER, **style))
         self.app = app
 
-        login_title = toga.Label(
-            "Login Menu",
-            style=Pack(padding=5, alignment=CENTER),
+        logo_path = os.path.join(self.app.paths.app, 'resources', 'logo.png')
+        logo = Image(logo_path)
+        logo_view = ImageView(
+            image=logo, 
+            style=Pack(height=100, width=100, padding=(20, 0))
+        )
+
+        welcome_label = toga.Label(
+            "Welcome to\nCoinFlip!", 
+            style=Pack(text_align=CENTER, font_size=24, padding=(10, 0))
         )
 
         login = toga.Button(
@@ -30,7 +39,7 @@ class LoginMenu(toga.Box):
             style=Pack(padding=5),
         )
 
-        self.add(login_title, login, register, login_guest)
+        self.add(logo_view, welcome_label, login, register, login_guest)
 
     def go_home(self, widget):
         self.app.show_home()
